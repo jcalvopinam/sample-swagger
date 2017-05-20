782ae405b88092f29cf62665789a174be9c1a571
Juan Calvopina <juan.calvopina@gmail.com>
2017-05-20 19:30:22 -0300

    Initial commit

---
+++ src/main/java/com/jcalvopinam/SampleSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.jcalvopinam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSwaggerApplication.class, args);
+    }
+
+}
