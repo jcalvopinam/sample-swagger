7e118822af9c5bcb68be07cf0c4d7dcd8bf0842e
Juan Calvopina <juan.calvopina@gmail.com>
2017-05-23 21:18:12 -0300

    Added documentation

--- src/main/java/com/jcalvopinam/controller/ContactController.java
+++ src/main/java/com/jcalvopinam/controller/ContactController.java
@@ -26,6 +26,10 @@ package com.jcalvopinam.controller;
 
 import com.jcalvopinam.domain.Contact;
 import com.jcalvopinam.service.ContactService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,6 +38,9 @@ import java.util.List;
 /**
  * Created by juanca on 5/18/17.
  */
+@Api(value = "ContactController Endpoint",
+        description = "Endpoint for managing contacts",
+        basePath = "personal-information/v1")
 @RestController
 @RequestMapping(value = "/personal-information/v1")
 public class ContactController {
@@ -41,26 +48,62 @@ public class ContactController {
     @Autowired
     private ContactService contactService;
 
+    @ApiOperation(value = "Retrieves all contacts.", responseContainer = "List<Contact>")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error occurred")})
     @GetMapping(value = "/contacts")
     public List<Contact> findAll() {
         return contactService.findAll();
     }
 
+    @ApiOperation(value = "Finds a contact by {id}.", responseContainer = "Contact")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error occurred")})
     @GetMapping(value = "/contact/{id}")
     public Contact findContact(@PathVariable("id") Long id) {
         return contactService.find(id);
     }
 
+    @ApiOperation(value = "Adds a new contact. It is necessary to fill in the attributes as shown in the 'Example Value'.",
+            responseContainer = "Contact")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error occurred")})
     @PostMapping(value = "/contact")
     public Contact createContact(@RequestBody Contact contact) {
         return contactService.save(contact);
     }
 
+    @ApiOperation(value = "Updates an existing contact by {id}.", responseContainer = "Contact")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error occurred")})
     @PutMapping(value = "/contact/{id}")
     public Contact updateContact(@PathVariable("id") Long id, @RequestBody Contact contact) {
         return contactService.update(id, contact);
     }
 
+    @ApiOperation(value = "Deletes an existing contact by {id}.", responseContainer = "void")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Internal server error occurred")})
     @DeleteMapping(value = "/contact/{id}")
     public void deleteContact(@PathVariable("id") Long id) {
         contactService.delete(id);
